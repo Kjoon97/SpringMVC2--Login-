49216d175192ab59f73c26bfa62cd29423453f22
joonhyuck <jh83370@naver.com>
2022-07-10 00:27:47 +0900

    ArgumentResolver

---
+++ src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,44 @@
+package hello.login.web.argumentresolver;
+
+import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        log.info("supportsParameter 실행");
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);   // @Login이 파라미터에 있느냐
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());  //@Login 뒤에 선언 되어있는 것이 Member 타입이냐.
+
+        return hasLoginAnnotation && hasMemberType;  //true를 반환하면 resolveArgument()이 호출 됨.
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        log.info("resolveArgument 실행");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return null;
+        }
+
+        Object member = session.getAttribute(SessionConst.LOGIN_MEMBER);
+        return member;
+    }
+}
+
