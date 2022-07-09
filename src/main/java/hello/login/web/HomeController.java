4aff7bfee3840b0eefa1bde005bfdc1ef5356f75
joonhyuck <jh83370@naver.com>
2022-07-09 21:17:26 +0900

    로그인 처리, 로그아웃 - 쿠키

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,43 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) { //로그인 안한 사용자도 들어와야하므로 required = false로.
+                                                                                                            // 쿠키 아이디 String에서 자동으로 Long으로 타입 변환 해줌.
+        if (memberId == null) {
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+
+        //디비에 없는 경우
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //있으면
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
