892b05758c2f28c038c37327770d3c3d723d5891
joonhyuck <jh83370@naver.com>
2022-07-10 00:03:51 +0900

    스프링 인터셉터 - 요청 로그

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,16 +1,30 @@
 package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+
+    //인터셉터 등록.
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")            //모든 경로 허용.
+                .excludePathPatterns("/css/**", "/*.ico","/error");   //제외 경로
+    }
 
     //LogFilter 등록
-    @Bean
+   // @Bean
     public FilterRegistrationBean logFilter() {
 
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
